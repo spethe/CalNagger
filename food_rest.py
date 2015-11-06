419372df1b50879ee951b9a32d204d610e49254f
spethe <swanand.pethe@gmail.com>
2015-11-06 04:04:38 -0800

    back to square 1

--- food_rest.py
+++ food_rest.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct 31 12:33:46 2015
-
 @author: swanand
 """
-import os
+import dweepy
 import requests
 import json
-import dweepy
 from flask import Flask
 from pymongo import MongoClient
-from flask.ext.cors import CORS
 app = Flask(__name__)
-cors = CORS(app)
-MONGO_URL='mongodb://admin:admin@ds049864.mongolab.com:49864/heroku_2xf72wpb?authMechanism=SCRAM-SHA-1'
 
 @app.route('/')
 def hello_world():
@@ -23,41 +18,43 @@ def hello_world():
 def getCaloriesForBarcode():
     url='http://world.openfoodfacts.org/api/v0/product/737628064502.json'
     data = requests.get(url)
-    #print os.environ.get("MONGOLAB_URI")
-    #dumpToMongo(data.json())
+
+    #info = dumpToMongo(data.json())
     info = dumpToDweet(data.json())
     return json.dumps(json.dumps(info),indent=4)
-
-
+    
 def dumpToMongo(data):
     info ={'user':'1',
            'genericName': data['product']['generic_name'],
            'code': data['code'],
            'calories':data['product']['nutriments']['energy']
           }
-    client = MongoClient('mongodb://admin:admin@ds049864.mongolab.com:49864/heroku_2xf72wpb?authMechanism=SCRAM-SHA-1')
-    if client!= None:
-        conColl = client['consumption']
-        conColl.update_one({'user':'1'},{'$set':info},True)
-
-def dumpToDweet(data):
-    dweetUrl = 'https://dweet.io:443/dweet/for/decisive-train/'
-    info ={'user':'1',
-           'genericName': data['product']['generic_name'],
-           'code': data['code'],
-           'calories':data['product']['nutriments']['energy']
-          }
-    print json.dumps(info)    
-    #dweepy.dweet_for('decisive-train', json.dumps(info));
-    resp = requests.post(dweetUrl, data=json.dumps(info))
-    
-    print json.dumps(resp)
+    client = MongoClient()
+    cnag = client['calnagger']
+    conColl = cnag['consumption']
+    conColl.find_one_and_update({'user':'1'},{'$set':info},True)
     return {'user':'1',
            'genericName': data['product']['generic_name'],
            'code': data['code'],
            'calories':data['product']['nutriments']['energy']
           }
-    
+
+def dumpToDweet(data):
+    #dweetUrl = 'http://dweet.io:80/dweet/for/decisive-train/'
+    info ={'user':'1',
+           'genericName': data['product']['generic_name'],
+           'code': data['code'],
+           'calories':data['product']['nutriments']['energy']
+          }
+   
+    dweepy.dweet_for('decisive-train', info)
+    #resp = requests.post(dweetUrl, data=json.dumps(info), headers=headers)
+    #print resp
+    return {'user':'1',
+           'genericName': data['product']['generic_name'],
+           'code': data['code'],
+           'calories':data['product']['nutriments']['energy']
+          }   
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
