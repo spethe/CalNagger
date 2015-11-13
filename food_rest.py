59c4113c18bcb1d301802cdf60fdfec68f84b87c
spethe <swanand.pethe@gmail.com>
2015-11-13 14:53:24 -0800

    added alarms in code and cleanup1

--- food_rest.py
+++ food_rest.py
@@ -11,12 +11,18 @@ from flask import Flask
 from flask import request
 from pymongo import MongoClient
 from urlparse import urlparse
-import datetime
+from datetime import datetime
 from bson.objectid import ObjectId
 
 OUNCE_TO_G = 28.3495
 app = Flask(__name__)
 MONGO_URL = os.environ.get('MONGOLAB_URI')
+
+userinfo = {
+'1':'peanut',
+'2':'gluten'
+}
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -28,28 +34,21 @@ def getCaloriesForBarcode():
     
     barcode = request.args.get('barcode', '00214036') 
     weight = request.args.get('weight','16')
+    user = request.args.get('user','1')
     wt_in_g = float(weight) * OUNCE_TO_G
     no_of_100g= wt_in_g/100
     url='http://world.openfoodfacts.org/api/v0/product/' + barcode + '.json'
+    
     print 'URL is ----' + url
-    print 'NO-100' + str(no_of_100g)
+    print 'NO-100   --- ' + str(no_of_100g)
     data = requests.get(url)
+    
     aggrData = dumpToMongo(data.json(),no_of_100g)
-    info = dumpToDweet(data.json(), aggrData)
+    info = dumpToDweet(data.json(), aggrData, user)
     return json.dumps(json.dumps(info),indent=4)
     
 def dumpToMongo(data, no_of_100g):
-    
-    info={}
-    info.setdefault('user','1')
-    info.setdefault('product',{})
-    info['product'].setdefault('generic_name','default-name')
-    info['product'].setdefault('nutriments', {})
-    info['product']['nutriments'].setdefault('energy',0)
-    info['product']['nutriments'].setdefault('fat_100g',0)
-    info['product']['nutriments'].setdefault('proteins_100g',0)
-    info['product']['nutriments'].setdefault('carbohydrates_100g',0)
-    info.setdefault('code','000000')
+    info = getDefaultInfo();
 
     info ={
             'user':'1',
@@ -72,10 +71,7 @@ def dumpToMongo(data, no_of_100g):
     conColl.insert(info)
     aggrDocs = getDocsForToday(conColl)
     doc = next(iter(aggrDocs), None)
-    doc.setdefault('caloriesForDay',0)
-    doc.setdefault('fatsForDay',0.0)
-    doc.setdefault('carbsForDay', 0.0)
-    doc.setdefault('proteinsForDay', 0.0)
+    doc = getDefaultAggrDoc(doc)
     
     return {'user':'1',
            'genericName': data['product']['product_name'],
@@ -87,19 +83,33 @@ def dumpToMongo(data, no_of_100g):
            'proteinsForToday':str(doc['proteinsForDay'])
           }
 
-def dumpToDweet(data, aggrData):
+
+def getDefaultAggrDoc(doc):
+    doc.setdefault('caloriesForDay',0)
+    doc.setdefault('fatsForDay',0.0)
+    doc.setdefault('carbsForDay', 0.0)
+    doc.setdefault('proteinsForDay', 0.0)
+    return doc;
+    
+def getDefaultInfo():
     info={}
     info.setdefault('user','1')
     info.setdefault('product',{})
     info['product'].setdefault('generic_name','default-name')
     info['product'].setdefault('nutriments', {})
     info['product']['nutriments'].setdefault('energy',0)
+    info['product']['nutriments'].setdefault('fat_100g',0)
+    info['product']['nutriments'].setdefault('proteins_100g',0)
+    info['product']['nutriments'].setdefault('carbohydrates_100g',0)
     info.setdefault('code','000000')
-    aggrData.setdefault('caloriesForToday', '0')
-    aggrData.setdefault('carbsForToday', '0')
-    aggrData.setdefault('fatForToday', '0')
-    aggrData.setdefault('proteinsForToday', '0')
+    return info
+    
+def dumpToDweet(data, aggrData, user):
 
+    alert = isProhibited(data,user)
+    info = getDefaultInfo()
+    aggrData = getDefaultAggrDoc(aggrData)
+    
     info ={ 'user':'1',
             'genericName': data['product']['product_name'],
             'code': data['code'],
@@ -108,31 +118,37 @@ def dumpToDweet(data, aggrData):
             'carbsForToday':aggrData['carbsForToday'],
             'fatForToday':aggrData['fatForToday'],
             'proteinsForToday':aggrData['proteinsForToday'],
-            'weekly_calories':'10,2,3,5,6,7,8'
+            'weekly_calories':'10,2,3,5,6,7,8',
+            'alert': alert
+            
           }
    
     dweepy.dweet_for('decisive-train', info)
     return info
 
+def isProhibited(data,user):
+    product = data['product']
+    product.setdefault('traces','')
+    traces = str(product.get('traces'))
+    if userinfo[user] == '':
+        return '';
+    
+    if traces.find(userinfo[user]):
+        alert=userinfo[user]
+    return alert
+    
 def getDocsForToday(conColl):
-    today = datetime.datetime.today()
-    print datetime.datetime.now()
+    today = datetime.today()
     aggr = conColl.aggregate(
         [
-        {'$match':{'_id': {'$gte': ObjectId.from_datetime(datetime.datetime(today.year,today.month,today.day))}}},
+        {'$match':{'_id': {'$gte': ObjectId.from_datetime(datetime(today.year,today.month,today.day))}}},
         {'$group':{ '_id' : None, 'caloriesForDay': { '$sum': '$calories' },
         'fatsForDay':{'$sum':'$fat'}, 'carbsForDay':{'$sum':'$carbohydrates'}, 'proteinsForDay':{'$sum':'$proteins'}}}
         ]
         )
     
-    '''results = conColl.find({
-                                    '_id': {
-                                    '$gte': ObjectId.from_datetime(datetime.datetime(today.year,today.month,today.day))                          
-    }})'''
-    
     aggrDocs = list(aggr)
     
-    #Add Users
     return aggrDocs
     
 if __name__ == '__main__':
